ef72ec9c544e13fca7713c4390ce5398a08fda83
kdyoda <luketshannon31@gmail.com>
2019-02-03 21:41:15 -0800

    moved to final stages of digital clock program

--- Lab08/WSplaybyplay.py
+++ Lab08/WSplaybyplay.py
@@ -1,1 +1,795 @@
-asdfasdf
+Dodgers-Red Sox World Series Simulation:
+
+
+========== Game 1 ==========
+
+
+Inning 0 - Red Sox
+
+M. Betts was out
+A. Benintendi was out
+S. Pearce was out
+
+Inning 0 - Dodgers
+
+B. Dozier was out
+J. Turner homered (J. Turner scored)
+D. Freese was out
+M. Machado was out
+
+Score: Red Sox 0 - Dodgers 1
+
+Inning 1 - Red Sox
+
+J. Martinez was out
+X. Bogaerts was out
+R. Devers was out
+
+Inning 1 - Dodgers
+
+C. Taylor was out
+M. Kemp was out
+E. Hernandez was out
+
+Score: Red Sox 0 - Dodgers 1
+
+Inning 2 - Red Sox
+
+I. Kinsler singled
+S. Leon was out
+J. Bradley Jr. was out
+M. Betts was out
+
+Inning 2 - Dodgers
+
+Y. Puig was out
+A. Barnes was out
+B. Dozier was out
+
+Score: Red Sox 0 - Dodgers 1
+
+Inning 3 - Red Sox
+
+A. Benintendi doubled
+S. Pearce singled
+J. Martinez doubled (A. Benintendi scored)
+X. Bogaerts was out
+R. Devers was out
+I. Kinsler was out
+
+Inning 3 - Dodgers
+
+J. Turner was out
+D. Freese was out
+M. Machado singled
+C. Taylor singled
+M. Kemp was out
+
+Score: Red Sox 1 - Dodgers 1
+
+Inning 4 - Red Sox
+
+S. Leon singled
+J. Bradley Jr. was out
+M. Betts was out
+A. Benintendi was out
+
+Inning 4 - Dodgers
+
+E. Hernandez was out
+Y. Puig was out
+A. Barnes singled
+B. Dozier was out
+
+Score: Red Sox 1 - Dodgers 1
+
+Inning 5 - Red Sox
+
+S. Pearce was out
+J. Martinez was out
+X. Bogaerts doubled
+R. Devers was out
+
+Inning 5 - Dodgers
+
+J. Turner was out
+D. Freese was out
+M. Machado was out
+
+Score: Red Sox 1 - Dodgers 1
+
+Inning 6 - Red Sox
+
+I. Kinsler was out
+S. Leon was out
+J. Bradley Jr. was out
+
+Inning 6 - Dodgers
+
+C. Taylor was out
+M. Kemp was out
+E. Hernandez was out
+
+Score: Red Sox 1 - Dodgers 1
+
+Inning 7 - Red Sox
+
+M. Betts singled
+A. Benintendi singled
+S. Pearce was out
+J. Martinez was out
+X. Bogaerts was out
+
+Inning 7 - Dodgers
+
+Y. Puig was out
+A. Barnes was out
+B. Dozier singled
+J. Turner singled
+D. Freese doubled (B. Dozier scored)
+M. Machado was out
+
+Score: Red Sox 1 - Dodgers 2
+
+Inning 8 - Red Sox
+
+R. Devers was out
+I. Kinsler doubled
+S. Leon was out
+J. Bradley Jr. was out
+
+Inning 8 - Dodgers
+
+C. Taylor singled
+M. Kemp was out
+E. Hernandez was out
+Y. Puig singled
+A. Barnes was out
+
+Score: Red Sox 1 - Dodgers 2
+
+========== Game 2 ==========
+
+
+Inning 0 - Red Sox
+
+M. Betts singled
+A. Benintendi was out
+S. Pearce was out
+J. Martinez was out
+
+Inning 0 - Dodgers
+
+B. Dozier was out
+J. Turner was out
+D. Freese singled
+M. Machado was out
+
+Score: Red Sox 0 - Dodgers 0
+
+Inning 1 - Red Sox
+
+X. Bogaerts was out
+R. Devers was out
+I. Kinsler was out
+
+Inning 1 - Dodgers
+
+C. Taylor was out
+M. Kemp singled
+E. Hernandez was out
+Y. Puig was out
+
+Score: Red Sox 0 - Dodgers 0
+
+Inning 2 - Red Sox
+
+S. Leon was out
+J. Bradley Jr. was out
+M. Betts singled
+A. Benintendi was out
+
+Inning 2 - Dodgers
+
+A. Barnes singled
+B. Dozier was out
+J. Turner was out
+D. Freese was out
+
+Score: Red Sox 0 - Dodgers 0
+
+Inning 3 - Red Sox
+
+S. Pearce was out
+J. Martinez singled
+X. Bogaerts was out
+R. Devers singled
+I. Kinsler was out
+
+Inning 3 - Dodgers
+
+M. Machado singled
+C. Taylor tripled (M. Machado scored)
+M. Kemp was out
+E. Hernandez was out
+Y. Puig was out
+
+Score: Red Sox 0 - Dodgers 1
+
+Inning 4 - Red Sox
+
+S. Leon was out
+J. Bradley Jr. doubled
+M. Betts doubled (J. Bradley Jr. scored)
+A. Benintendi was out
+S. Pearce was out
+
+Inning 4 - Dodgers
+
+A. Barnes was out
+B. Dozier was out
+J. Turner doubled
+D. Freese was out
+
+Score: Red Sox 1 - Dodgers 1
+
+Inning 5 - Red Sox
+
+J. Martinez was out
+X. Bogaerts singled
+R. Devers was out
+I. Kinsler singled
+S. Leon was out
+
+Inning 5 - Dodgers
+
+M. Machado homered (M. Machado scored)
+C. Taylor was out
+M. Kemp was out
+E. Hernandez singled
+Y. Puig was out
+
+Score: Red Sox 1 - Dodgers 2
+
+Inning 6 - Red Sox
+
+J. Bradley Jr. tripled
+M. Betts was out
+A. Benintendi was out
+S. Pearce was out
+
+Inning 6 - Dodgers
+
+A. Barnes was out
+B. Dozier was out
+J. Turner was out
+
+Score: Red Sox 1 - Dodgers 2
+
+Inning 7 - Red Sox
+
+J. Martinez was out
+X. Bogaerts was out
+R. Devers was out
+
+Inning 7 - Dodgers
+
+D. Freese was out
+M. Machado singled
+C. Taylor singled
+M. Kemp was out
+E. Hernandez was out
+
+Score: Red Sox 1 - Dodgers 2
+
+Inning 8 - Red Sox
+
+I. Kinsler singled
+S. Leon was out
+J. Bradley Jr. doubled
+M. Betts was out
+A. Benintendi singled (I. Kinsler scored)
+S. Pearce homered (S. Pearce scored) (A. Benintendi scored) (J. Bradley Jr. scored)
+J. Martinez singled
+X. Bogaerts doubled
+R. Devers was out
+
+Inning 8 - Dodgers
+
+Y. Puig was out
+A. Barnes singled
+B. Dozier was out
+J. Turner doubled
+D. Freese singled (A. Barnes scored)
+M. Machado was out
+
+Score: Red Sox 5 - Dodgers 3
+
+========== Game 3 ==========
+
+
+Inning 0 - Red Sox
+
+M. Betts homered (M. Betts scored)
+A. Benintendi was out
+S. Pearce was out
+J. Martinez was out
+
+Inning 0 - Dodgers
+
+B. Dozier was out
+J. Turner was out
+D. Freese homered (D. Freese scored)
+M. Machado was out
+
+Score: Red Sox 1 - Dodgers 1
+
+Inning 1 - Red Sox
+
+X. Bogaerts was out
+R. Devers was out
+I. Kinsler singled
+S. Leon was out
+
+Inning 1 - Dodgers
+
+C. Taylor was out
+M. Kemp was out
+E. Hernandez was out
+
+Score: Red Sox 1 - Dodgers 1
+
+Inning 2 - Red Sox
+
+J. Bradley Jr. was out
+M. Betts singled
+A. Benintendi was out
+S. Pearce was out
+
+Inning 2 - Dodgers
+
+Y. Puig was out
+A. Barnes was out
+B. Dozier was out
+
+Score: Red Sox 1 - Dodgers 1
+
+Inning 3 - Red Sox
+
+J. Martinez homered (J. Martinez scored)
+X. Bogaerts was out
+R. Devers was out
+I. Kinsler was out
+
+Inning 3 - Dodgers
+
+J. Turner was out
+D. Freese was out
+M. Machado singled
+C. Taylor was out
+
+Score: Red Sox 2 - Dodgers 1
+
+Inning 4 - Red Sox
+
+S. Leon singled
+J. Bradley Jr. was out
+M. Betts singled
+A. Benintendi was out
+S. Pearce singled
+J. Martinez was out
+
+Inning 4 - Dodgers
+
+M. Kemp was out
+E. Hernandez was out
+Y. Puig was out
+
+Score: Red Sox 2 - Dodgers 1
+
+Inning 5 - Red Sox
+
+X. Bogaerts was out
+R. Devers was out
+I. Kinsler was out
+
+Inning 5 - Dodgers
+
+A. Barnes was out
+B. Dozier was out
+J. Turner was out
+
+Score: Red Sox 2 - Dodgers 1
+
+Inning 6 - Red Sox
+
+S. Leon was out
+J. Bradley Jr. was out
+M. Betts singled
+A. Benintendi was out
+
+Inning 6 - Dodgers
+
+D. Freese was out
+M. Machado was out
+C. Taylor was out
+
+Score: Red Sox 2 - Dodgers 1
+
+Inning 7 - Red Sox
+
+S. Pearce was out
+J. Martinez was out
+X. Bogaerts was out
+
+Inning 7 - Dodgers
+
+M. Kemp was out
+E. Hernandez singled
+Y. Puig was out
+A. Barnes was out
+
+Score: Red Sox 2 - Dodgers 1
+
+Inning 8 - Red Sox
+
+R. Devers was out
+I. Kinsler was out
+S. Leon was out
+
+Inning 8 - Dodgers
+
+B. Dozier was out
+J. Turner was out
+D. Freese was out
+
+Score: Red Sox 2 - Dodgers 1
+
+========== Game 4 ==========
+
+
+Inning 0 - Red Sox
+
+M. Betts was out
+A. Benintendi was out
+S. Pearce was out
+
+Inning 0 - Dodgers
+
+B. Dozier was out
+J. Turner was out
+D. Freese singled
+M. Machado was out
+
+Score: Red Sox 0 - Dodgers 0
+
+Inning 1 - Red Sox
+
+J. Martinez was out
+X. Bogaerts singled
+R. Devers was out
+I. Kinsler was out
+
+Inning 1 - Dodgers
+
+C. Taylor was out
+M. Kemp was out
+E. Hernandez singled
+Y. Puig was out
+
+Score: Red Sox 0 - Dodgers 0
+
+Inning 2 - Red Sox
+
+S. Leon was out
+J. Bradley Jr. was out
+M. Betts singled
+A. Benintendi singled
+S. Pearce doubled (M. Betts scored)
+J. Martinez doubled (S. Pearce scored) (A. Benintendi scored)
+X. Bogaerts was out
+
+Inning 2 - Dodgers
+
+A. Barnes was out
+B. Dozier singled
+J. Turner singled
+D. Freese was out
+M. Machado was out
+
+Score: Red Sox 3 - Dodgers 0
+
+Inning 3 - Red Sox
+
+R. Devers was out
+I. Kinsler singled
+S. Leon was out
+J. Bradley Jr. was out
+
+Inning 3 - Dodgers
+
+C. Taylor doubled
+M. Kemp was out
+E. Hernandez was out
+Y. Puig doubled (C. Taylor scored)
+A. Barnes was out
+
+Score: Red Sox 3 - Dodgers 1
+
+Inning 4 - Red Sox
+
+M. Betts singled
+A. Benintendi singled
+S. Pearce singled
+J. Martinez was out
+X. Bogaerts was out
+R. Devers was out
+
+Inning 4 - Dodgers
+
+B. Dozier was out
+J. Turner singled
+D. Freese singled
+M. Machado was out
+C. Taylor was out
+
+Score: Red Sox 3 - Dodgers 1
+
+Inning 5 - Red Sox
+
+I. Kinsler was out
+S. Leon was out
+J. Bradley Jr. singled
+M. Betts was out
+
+Inning 5 - Dodgers
+
+M. Kemp was out
+E. Hernandez was out
+Y. Puig was out
+
+Score: Red Sox 3 - Dodgers 1
+
+Inning 6 - Red Sox
+
+A. Benintendi singled
+S. Pearce was out
+J. Martinez was out
+X. Bogaerts was out
+
+Inning 6 - Dodgers
+
+A. Barnes doubled
+B. Dozier was out
+J. Turner was out
+D. Freese was out
+
+Score: Red Sox 3 - Dodgers 1
+
+Inning 7 - Red Sox
+
+R. Devers was out
+I. Kinsler was out
+S. Leon was out
+
+Inning 7 - Dodgers
+
+M. Machado singled
+C. Taylor doubled
+M. Kemp doubled (C. Taylor scored) (M. Machado scored)
+E. Hernandez was out
+Y. Puig was out
+A. Barnes was out
+
+Score: Red Sox 3 - Dodgers 3
+
+Inning 8 - Red Sox
+
+J. Bradley Jr. was out
+M. Betts homered (M. Betts scored)
+A. Benintendi was out
+S. Pearce was out
+
+Inning 8 - Dodgers
+
+B. Dozier was out
+J. Turner was out
+D. Freese was out
+
+Score: Red Sox 4 - Dodgers 3
+
+========== Game 5 ==========
+
+
+Inning 0 - Red Sox
+
+M. Betts was out
+A. Benintendi was out
+S. Pearce was out
+
+Inning 0 - Dodgers
+
+B. Dozier was out
+J. Turner singled
+D. Freese was out
+M. Machado was out
+
+Score: Red Sox 0 - Dodgers 0
+
+Inning 1 - Red Sox
+
+J. Martinez singled
+X. Bogaerts homered (X. Bogaerts scored) (J. Martinez scored)
+R. Devers was out
+I. Kinsler was out
+S. Leon was out
+
+Inning 1 - Dodgers
+
+C. Taylor was out
+M. Kemp was out
+E. Hernandez was out
+
+Score: Red Sox 2 - Dodgers 0
+
+Inning 2 - Red Sox
+
+J. Bradley Jr. was out
+M. Betts singled
+A. Benintendi was out
+S. Pearce was out
+
+Inning 2 - Dodgers
+
+Y. Puig was out
+A. Barnes singled
+B. Dozier was out
+J. Turner was out
+
+Score: Red Sox 2 - Dodgers 0
+
+Inning 3 - Red Sox
+
+J. Martinez was out
+X. Bogaerts singled
+R. Devers was out
+I. Kinsler singled
+S. Leon was out
+
+Inning 3 - Dodgers
+
+D. Freese singled
+M. Machado was out
+C. Taylor homered (C. Taylor scored) (D. Freese scored)
+M. Kemp singled
+E. Hernandez was out
+Y. Puig was out
+
+Score: Red Sox 2 - Dodgers 2
+
+Inning 4 - Red Sox
+
+J. Bradley Jr. was out
+M. Betts was out
+A. Benintendi was out
+
+Inning 4 - Dodgers
+
+A. Barnes singled
+B. Dozier was out
+J. Turner was out
+D. Freese singled
+M. Machado was out
+
+Score: Red Sox 2 - Dodgers 2
+
+Inning 5 - Red Sox
+
+S. Pearce was out
+J. Martinez was out
+X. Bogaerts tripled
+R. Devers was out
+
+Inning 5 - Dodgers
+
+C. Taylor was out
+M. Kemp was out
+E. Hernandez was out
+
+Score: Red Sox 2 - Dodgers 2
+
+Inning 6 - Red Sox
+
+I. Kinsler singled
+S. Leon was out
+J. Bradley Jr. was out
+M. Betts was out
+
+Inning 6 - Dodgers
+
+Y. Puig was out
+A. Barnes was out
+B. Dozier was out
+
+Score: Red Sox 2 - Dodgers 2
+
+Inning 7 - Red Sox
+
+A. Benintendi was out
+S. Pearce was out
+J. Martinez singled
+X. Bogaerts was out
+
+Inning 7 - Dodgers
+
+J. Turner was out
+D. Freese was out
+M. Machado was out
+
+Score: Red Sox 2 - Dodgers 2
+
+Inning 8 - Red Sox
+
+R. Devers was out
+I. Kinsler singled
+S. Leon singled
+J. Bradley Jr. was out
+M. Betts was out
+
+Inning 8 - Dodgers
+
+C. Taylor was out
+M. Kemp was out
+E. Hernandez was out
+
+Score: Red Sox 2 - Dodgers 2
+
+Inning 9 - Red Sox
+
+A. Benintendi was out
+S. Pearce was out
+J. Martinez doubled
+X. Bogaerts singled
+R. Devers was out
+
+Inning 9 - Dodgers
+
+Y. Puig singled
+A. Barnes was out
+B. Dozier was out
+J. Turner was out
+
+Score: Red Sox 2 - Dodgers 2
+
+Inning 10 - Red Sox
+
+I. Kinsler was out
+S. Leon was out
+J. Bradley Jr. was out
+
+Inning 10 - Dodgers
+
+D. Freese was out
+M. Machado was out
+C. Taylor was out
+
+Score: Red Sox 2 - Dodgers 2
+
+Inning 11 - Red Sox
+
+M. Betts was out
+A. Benintendi singled
+S. Pearce doubled
+J. Martinez was out
+X. Bogaerts homered (X. Bogaerts scored) (S. Pearce scored) (A. Benintendi scored)
+R. Devers was out
+
+Inning 11 - Dodgers
+
+M. Kemp homered (M. Kemp scored)
+E. Hernandez was out
+Y. Puig was out
+A. Barnes doubled
+B. Dozier was out
+
+Score: Red Sox 5 - Dodgers 3
--- Lab09_Lists/Lab09_DigitalClock.py
+++ Lab09_Lists/Lab09_DigitalClock.py
@@ -11,63 +11,186 @@ class Cell:
 
     """ This is the description. """
 
-    def __init__(self, center, orientation=False):
+    def __init__(self, gui, center, orientation='x'):
+        self.gui = gui
+        self.win = gui.win
         self.center = center
         self.orientation = orientation
+        x = self.center.getX()
+        y = self.center.getY()
         if self.orientation:
-            x = self.center.getX()
-            y = self.center.getY()
             self.cell = Polygon(Point(x+45, y), Point(x+40, y+5), Point(x-40, y+5),
                                 Point(x-45, y), Point(x-40, y-5), Point(x+40, y-5))
         else:
-            y = self.center.getX()
-            x = self.center.getY()
-            self.cell = Polygon(Point(x+45, y), Point(x+40, y+5), Point(x-40, y+5),
-                                Point(x-45, y), Point(x-40, y-5), Point(x+40, y-5))
+            self.cell = Polygon(Point(x, y-45), Point(x+5, y-40), Point(x+5, y+40),
+                                Point(x, y+45), Point(x-5, y+40), Point(x-5, y-40))
+        self.cell.setFill('white')
+        self.cell.draw(self.win)
 
     def fill(self):
-        self.cell.fill('black')
+        self.cell.setFill('black')
 
     def unfill(self):
-        self.cell.fill('white')
-        self.cell.setOutline('white')
+        self.cell.setFill('white')
+
 
 class Digit:
 
     """ This is the description. """
 
-    def __init__(self, cellList):
-        self.cellList = cellList
-        self.numberList =  {0 : [0, 1, 2, 3, 4, 5], 1 : [1, 2], 2: [0, 1, 6, 4, 3], 3 : [0, 1, 6, 2, 3],
-                            4 : [5, 6, 1, 2], 5 : [0, 5, 6, 2, 3], 6: [0, 5, 6, 2, 3, 4], 7 : [0, 1, 2],
-                            8 : [0, 1, 2, 3, 4, 5, 6], 9 : [0, 1, 2, 5, 6]}
+    def __init__(self, center, gui):
+        self.gui = gui
+        self.center = center
+        self.number = 0
 
-    def setTime(self, number):
-        cellsToFill = self.numberList(number)
+        self.cell0 = Cell(self.gui, Point(center.getX(), center.getY() - 90))
+        self.cell1 = Cell(self.gui, Point(center.getX() + 45, center.getY() - 45), False)
+        self.cell2 = Cell(self.gui, Point(center.getX() + 45, center.getY() + 45), False)
+        self.cell3 = Cell(self.gui, Point(center.getX(), center.getY() + 90))
+        self.cell4 = Cell(self.gui, Point(center.getX() - 45, center.getY() + 45), False)
+        self.cell5 = Cell(self.gui, Point(center.getX() - 45, center.getY() - 45), False)
+        self.cell6 = Cell(self.gui, Point(center.getX(), center.getY()))
+
+        self.numberList = {0: [0, 1, 2, 3, 4, 5],       1: [1, 2],              2: [0, 1, 6, 4, 3],     3: [0, 1, 6, 2, 3],
+                           4: [5, 6, 1, 2],             5: [0, 5, 6, 2, 3],     6: [0, 5, 6, 2, 3, 4],  7: [0, 1, 2],
+                           8: [0, 1, 2, 3, 4, 5, 6],    9: [0, 1, 2, 5, 6]}
+
+    def setTime(self):
+        cellsToFill = self.numberList[self.number]
+        for num in range(7):
+            eval('self.cell' + str(num)).unfill()
         for num in cellsToFill:
-            eval('cell'+str(num)).fill()
+            eval('self.cell'+str(num)).fill()
+
 
 class GUI:
 
     """ This is the description. """
 
     def __init__(self):
-        self.win = GraphWin('Alarm Clock', 600, 500)
-        self.outside = Rectangle(Point(50, 50), Point(550, 200))
-        self.entry = Entry(Point(300, 225), 20)
-        self.upHour = Button(self.win, Point(50, 350), 100, 40, '+ HOUR', 'black')
-        self.downHour = Button(self.win, Point(50, 420), 100, 40, '- HOUR', 'black')
-        self.upMinute = Button(self.win, Point(200, 350), 100, 40, '+ MINUTE', 'black')
-        self.downMinute = Button(self.win, Point(200, 420), 100, 40, '- MINUTE', 'black')
-        self.change = Button(self.win, Point(350, 350), 100, 40, 'CHANGE', 'black')
-        self.quit = Button(self.win, Point(350, 420), 100, 40, 'QUIT', 'black')
+        self.win = GraphWin('Alarm Clock', 500, 500)
+        self.entry = Entry(Point(250, 300), 20)
+        self.entry.draw(self.win)
+        self.secondTop = Rectangle(Point(245, 145), Point(255, 155))
+        self.secondBottom = Rectangle(Point(245, 95), Point(255, 105))
+        self.secondTop.setFill('black')
+        self.secondTop.draw(self.win)
+        self.secondBottom.setFill('black')
+        self.secondBottom.draw(self.win)
+        self.upHour = Button(self.win, Point(50, 350), 100, 40, '+ HOUR', 'light blue')
+        self.downHour = Button(self.win, Point(50, 420), 100, 40, '- HOUR', 'light blue')
+        self.upMinute = Button(self.win, Point(200, 350), 100, 40, '+ MINUTE', 'light blue')
+        self.downMinute = Button(self.win, Point(200, 420), 100, 40, '- MINUTE', 'light blue')
+        self.change = Button(self.win, Point(350, 350), 100, 40, 'CHANGE', 'light blue')
+        self.quit = Button(self.win, Point(350, 420), 100, 40, 'QUIT', 'light blue')
+        self.buttons = [self.upHour, self.downHour, self.upMinute, self.downMinute, self.change, self.quit]
+        for button in self.buttons:
+            button.activate()
+        self.am = True
+        self.amText = Text(Point(430, 250), '')
+        self.amText.setSize(24)
+        self.amText.draw(self.win)
+        self.changeAMPM(self.am)
 
-    def
+    def changeAMPM(self, am):
+        if am:
+            self.amText.setText('AM')
+        else:
+            self.amText.setText('PM')
 
-def initializeDigits():
-    for thing in '':
-        print ('hi')
+
+def initializeDigits(gui):
+    centers = [Point(70, 125), Point(180, 125), Point(320, 125), Point(430, 125)]
+    hour1 = Digit(centers[0], gui)
+    hour2 = Digit(centers[1], gui)
+    minute1 = Digit(centers[2], gui)
+    minute2 = Digit(centers[3], gui)
+    return hour1, hour2, minute1, minute2
+
+
+def checkHour(gui, hour1, hour2, am):
+    if hour2.number == 10:
+        hour2.number = 0
+        hour1.number = 1
+    elif hour2.number == 3 and hour1.number == 1:
+        hour1.number = 0
+        hour2.number = 1
+    if hour1.number == 0 and hour2.number == 0:
+        hour2.number = 9
+    if hour1.number == 1 and hour2.number == 2:
+        am[0] = not am[0]
+        gui.changeAMPM(am[0])
+
+
+def checkMinute(minute1, minute2, hour1, hour2, am, gui):
+    if minute2.number == 10:
+        minute2.number = 0
+        minute1.number += 1
+    if minute1.number == 6:
+        hour2.number += 1
+        minute1.number = 0
+        minute2.number = 0
+        checkHour(gui, hour1, hour2, am)
+
+
+def takeEntry(str, hour1, hour2, minute1, minute2):
+    error = False
+    for ch in str:
+        if not (ch.isdigit() or ch == ':'):
+            error = True
+    if not error:
+        numbers = str.split(':')
+        if not len(numbers) == 2:
+            error = True
+        if not (len(numbers[0]) == 1 or len(numbers[0]) == 2) or not(len(numbers[0]) == 1 or len(numbers[0]) == 2):
+            error = True
+    if not error:
+        hours = numbers[0]
+        if len(hours) == 1:
+            hour1.number = 0
+            hour2.number = eval(hours[0])
+        elif len(hours) == 2:
+            hour1.number = eval(hours[0])
+            hour2.number = eval(hours[1])
+        minutes = numbers[1]
+        if len(minutes) == 1:
+            minute2.number = 0
+            minute2.number = eval(minutes[0])
+        elif len(minutes) == 2:
+            minute1.number = eval(minutes[0])
+            minute2.number = eval(minutes[1])
+
 
 def main():
     gui = GUI()
-    initializeDigits()
+    hour1, hour2, minute1, minute2 = initializeDigits(gui)
+    hour1.number, hour2.number = 1, 2
+    m1 = Point(0, 0)
+    am = [False]
+    hour1.setTime()
+    hour2.setTime()
+    minute1.setTime()
+    minute2.setTime()
+    while not gui.quit.clicked(m1):
+        if gui.upHour.clicked(m1):
+            hour2.number += 1
+        if gui.upMinute.clicked(m1):
+            minute2.number += 1
+        if gui.downHour.clicked(m1):
+            minute2.number += -1
+        if gui.downMinute.clicked(m1):
+            minute2.number += -1
+        if gui.change.clicked(m1):
+            takeEntry(gui.entry.getText(), hour1, hour2, minute1, minute2)
+            gui.entry.setText('')
+        checkHour(gui, hour1, hour2, am)
+        checkMinute(minute1, minute2, hour1, hour2, am, gui)
+        hour1.setTime()
+        hour2.setTime()
+        minute1.setTime()
+        minute2.setTime()
+        m1 = gui.win.getMouse()
+
+
+main()
+
