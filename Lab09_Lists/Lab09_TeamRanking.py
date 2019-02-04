71402699ab5d5c3ec11b1ffe3fd391f9fc3adaa7
kdyoda <47166319+kdyoda@users.noreply.github.com>
2019-02-03 22:54:26 -0800

    began work on teamRanking.py

--- Lab09_Lists/Lab09_TeamRanking.py
+++ Lab09_Lists/Lab09_TeamRanking.py
@@ -3,12 +3,55 @@
 # 01/29/19
 # This program creates team rankings.
 
-from Lab09_graphics import *
+class Team:
 
-win = GraphWin('s', 400,400)
-l = Line(Point(20,20),Point(100,20))
-t = Line(Point(20,20), Point(20,30))
-l.draw(win)
-t.draw(win)
-win.getMouse()
+    """ This is the description. """
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def addGame(self, line):
+        teams = line.split(' vs ')
+        team1 = teams[0]
+        team2 = teams[1]
+        if str(team) in team1:
+
+
+    def scoreGames(self):
+
+
+
+def inputs():
+    filename = input("What would you like to name the file (TeamRankingInput.py)? ") or 'TeamRankingInput.py'
+    ranking = input("According to what metric (wins, draws, losses)? ") or "wins"
+    trend = input("According to what trend (greatest to least)? ") or "greatest to least"
+    return filename, ranking, trend
+
+
+def readFile(filename):
+    infile = open(filename, 'r')
+    contents = [infile.readline(), infile.read]
+    contents[1].split('\n\n')
+    league = contents[0]
+    teamList = contents[1]
+    teamList.split('\n')
+    scores = contents[2]
+    scores.split('\n')
+    return league, teamList, scores
+
+
+
+def main():
+    filename, ranking, trend = inputs()
+    league, teamList, scores = readFile(filename)
+    allTeams = []
+    for team in teamList:
+        allTeams.append(Team(team))
+    for score in scores:
+        for team in allTeams:
+            if str(team) in score:
+                team.addGame(score)
 
---
+++ Lab09_Lists/TeamRankingInput.py
@@ -0,0 +1,12 @@
+Under-6 Girls Soccer League
+Red Cats
+Blue Dogs
+Yellow Birds
+Green Fish
+
+Red Cats 2 vs Blue Dogs 1
+Yellow Birds 3 vs Green Fish 3
+Blue Dogs 0 vs Yellow Birds 1
+Green Fish 4 vs Red Cats 2
+Yellow Birds 2 vs Red Cats 2
+Blue Dogs 1 vs Green Fish 1
